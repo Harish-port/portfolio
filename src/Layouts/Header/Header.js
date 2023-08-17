cd542887678012fe05dd2d45d9bf8fa8c271420d
Harish-port <harish.sq@gmail.com>
2023-08-17 17:48:54 +0530

    Header component

---
+++ src/Layouts/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <p>Home</p>
+        <p>About Myself</p>
+        <p>Skills</p>
+        <p>Certifications</p>
+        <p>Projects</p>
+        <p>Contact</p>
+    </div>
+  )
+}
+
+export default Header;
