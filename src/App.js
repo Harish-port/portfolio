0c799a603e50fc945eaa82175e03b8ad253eac77
Harish-port <harish.sq@gmail.com>
2023-08-19 00:03:36 +0530

    Main section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './Layouts/Header/Header';
 import './App.css';
+import Main from './Layouts/MainIntro/Main';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Main/>
     </div>
   );
 }
---
+++ src/Layouts/MainIntro/Main.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Main.scss';
+
+function Main() {
+    return (
+        <div className='main-container'>
+            <div className="content">
+                <div className="hero-main">
+                    <div className="hero-text">
+                    <h1>
+                        Front-End React Developer
+                    </h1>
+                    <img src="https://stefantopalovicdev.vercel.app/static/media/waving.1bae5fcfb51082b5c2b4.png" alt=""/>
+                    <p>
+                    Hi, I'm Harish S. A passionate Front-end React Developer based in Bengaluru. 📍
+                    </p>
+                    </div>
+                </div>
+                <div className="skills">
+                    Skills
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
