891df5e735e0f5418252b55e8a16000087074b5c
Harish-port <harish.sq@gmail.com>
2023-08-18 14:09:53 +0530

    header style

--- src/Layouts/Header/Header.js
+++ src/Layouts/Header/Header.js
@@ -3,31 +3,39 @@ import './Header.scss';
 
 function Header() {
   return (
-    <nav className='header-container'>
-      <h3 className='logo'>Harish.dev</h3>
-      <ul>
-        <li>
-          <a href='#home'>
-          </a>
-          Home
-        </li>
-        <li>
-          <a href='#home'>
-          </a>
-          About
-        </li>
-        <li>
-          <a href='#home'>
-          </a>
-          Projects
-        </li>
-        <li>
-          <a href='#home'>
-          </a>
-          Contact
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className='header-container'>
+        <h3 className='logo'>Harish.dev</h3>
+        {/* Hamburger icon  */}
+        <input class="side-menu" type="checkbox" id="side-menu"/>
+        <label class="hamb" for="side-menu">
+          <span class="hamb-line"></span>
+          </label>
+        <ul>
+          <li>
+            <a href='#home'>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href='#home'>
+              About
+            </a>
+          </li>
+          <li>
+            <a href='#home'>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href='#home'>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    
+    </>
   )
 }
 
