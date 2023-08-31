bc04fb77921581e1aee3c075f41613270accbea8
Harish S <harish.port25@gmail.com>
2023-08-31 15:15:35 +0530

    Project section modified

--- src/Layouts/Projects/Projects.jsx
+++ src/Layouts/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Projects.scss';
+import data from '../../Utils/data.json';
 
 function Projects() {
     return (
@@ -9,63 +10,29 @@ function Projects() {
                     <p>PORTFOLIO</p>
                     <h4 className='heading'>Each project is a unique piece of development 🧩</h4>
                     <div className="projects-list-wrapper">
-                        <div className='projects-list'>
-                            <div className='project-display'>
-                                <img src='https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGRlc2slMjBzZXR1cHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80' alt='NA' />
-                            </div>
-                            <div className="project-info">
-                                <h3 className='project-title'>Project Title</h3>
-                                <p className="project-description">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat dolorem officia suscipit un   de voluptate labore omnis eum, sapiente quam est.
-                                </p>
-                                <div className='tech-stack'>
-                                    <p className='tech1'>React</p>
-                                    <p className='tech2'>SAss</p>
+                        {
+                            data.projects.map(({title,description,techstack,links}, i) => (
+                                <div className='projects-list'key={i} >
+                                    <div className='project-display'>
+                                        <img src='https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGRlc2slMjBzZXR1cHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80' alt='NA' />
+                                    </div>
+                                    <div className="project-info">
+                                        <h3 className='project-title'>{title}</h3>
+                                        <p className="project-description">
+                                            {description}
+                                        </p>
+                                        <div className='tech-stack'>
+                                            <p className='tech1'>{techstack[0]}</p>
+                                            <p className='tech2'>{techstack[1]}</p>
+                                        </div>
+                                        <div className="project-source-view">
+                                            <a target="_blank" href={links.sourcecode} rel="noreferrer">Code <i className="fa-brands fa-github"></i></a>
+                                            <a target="_blank" href={links.livedemolink} rel="noreferrer">Live Demo<i className="fa-solid fa-arrow-up-right-from-square link-icon"></i></a>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="project-source-view">
-                                    <a target="_blank" href="https://github.com/Harish-port" rel="noreferrer">Code <i class="fa-brands fa-github"></i></a>
-                                    <a target="_blank" href="https://github.com/Harish-port" rel="noreferrer">Live Demo<i class="fa-solid fa-arrow-up-right-from-square link-icon"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='projects-list'>
-                            <div className='project-display'>
-                                <img src='https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGRlc2slMjBzZXR1cHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80' alt='NA' />
-                            </div>
-                            <div className="project-info">
-                                <h3 className='project-title'>Project Title</h3>
-                                <p className="project-description">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat dolorem officia suscipit un   de voluptate labore omnis eum, sapiente quam est.
-                                </p>
-                                <div className='tech-stack'>
-                                    <p className='tech1'>React</p>
-                                    <p className='tech2'>SAss</p>
-                                </div>
-                                <div className="project-source-view">
-                                    <a target="_blank" href="https://github.com/Harish-port" rel="noreferrer">Code <i class="fa-brands fa-github"></i></a>
-                                    <a target="_blank" href="https://github.com/Harish-port" rel="noreferrer">Live Demo<i class="fa-solid fa-arrow-up-right-from-square link-icon"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='projects-list'>
-                            <div className='project-display'>
-                                <img src='https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGRlc2slMjBzZXR1cHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80' alt='NA' />
-                            </div>
-                            <div className="project-info">
-                                <h3 className='project-title'>Project Title</h3>
-                                <p className="project-description">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat dolorem officia suscipit un   de voluptate labore omnis eum, sapiente quam est.
-                                </p>
-                                <div className='tech-stack'>
-                                    <p className='tech1'>React</p>
-                                    <p className='tech2'>SAss</p>
-                                </div>
-                                <div className="project-source-view">
-                                    <a target="_blank" href="https://github.com/Harish-port" rel="noreferrer">Code <i class="fa-brands fa-github"></i></a>
-                                    <a target="_blank" href="https://github.com/Harish-port" rel="noreferrer">Live Demo<i class="fa-solid fa-arrow-up-right-from-square link-icon"></i></a>
-                                </div>
-                            </div>
-                        </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
